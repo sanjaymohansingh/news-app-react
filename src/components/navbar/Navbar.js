9af6a0e9bb99762c72b6e4b582aea43f3619ade6
sanjaymohansingh <sanjaysingh19p@gmail.com>
2023-12-06 06:29:17 -0800

    changing navbar style

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,23 +1,33 @@
-import pageStyle from  './navbar.module.css';
+import pageStyle from "./navbar.module.css";
 
-import React, { useState } from 'react';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
-import Profile from './Profile';
-import { useDispatch } from 'react-redux';
-import { fetchData,onCategoryClick } from '../../redux/reducers/userReducer';
+import Profile from "./Profile";
+import { useDispatch } from "react-redux";
+import { fetchData, onCategoryClick } from "../../redux/reducers/userReducer";
+import Logo from "../../../src/assets/logo.png";
 
 const Navbar = () => {
-  const categoriesArray = ["Politics", "Sports", "Education", "Tech", "Business", "Entertainment","Lifestyle","Research","Investigations","Elections","Terrorism","IPL","Finance", "Careers","Movies","Music"];
-  const[showProfile, setShowProfile] = useState(false);
-  const [searchText, setSearchText] = useState('');
+  const categoriesArray = [
+    "Investigations",
+    "Elections",
+    "Terrorism",
+    "IPL",
+    "Finance",
+    "Careers",
+    "Movies",
+    "Music",
+  ];
+  const [showProfile, setShowProfile] = useState(false);
+  const [searchText, setSearchText] = useState("");
   const location = useLocation();
   const dispatch = useDispatch();
-  
-  function handleSubmit(e){
+
+  function handleSubmit(e) {
     e.preventDefault();
-    if(searchText===''){
-      dispatch(fetchData('world'));
+    if (searchText === "") {
+      dispatch(fetchData("world"));
       return;
     }
     dispatch(fetchData(searchText));
@@ -25,48 +35,77 @@ const Navbar = () => {
 
   return (
     <>
-        <nav>
-                <div className={pageStyle.navbar}>
-                    <div className={pageStyle.container}>
-
-                      <div className={pageStyle.logo}>
-                        <a href='/'>
-                            <div className={pageStyle.brandName}>NEWS TIMES </div>
-                            <span> The only news you need to know </span>
-                        </a>
-                      </div>
-
-                      {location.pathname==='/'?
-                      <form className={pageStyle.searchBar} onSubmit={handleSubmit}>
-                        <input id="searchText" type="search" className={pageStyle.searchBox} placeholder="Search here.." 
-                              onChange={(e) => {
-                                setSearchText(e.target.value)
-                              }} 
-                            />
-                        <button id="searchBtn" className={pageStyle.searchBtn}><i className="fa-solid fa-magnifying-glass"></i></button>
-                      </form>
-                      :null} 
-
-                      <div className={pageStyle.homeIcon}> 
-                        {location.pathname==='/favorites'? <Link to='/'> <i className="fa-solid fa-house"></i> </Link>:null} 
-                      </div>
-
-                      <div className={pageStyle.profile} onMouseEnter={() => {setShowProfile(true)}} onMouseLeave={() => setShowProfile(false)}>
-                        <i className="fa-solid fa-circle-user"></i>
-                      </div>
-                      
-                      {showProfile?<Profile onMouseEnter={() => setShowProfile(true)} onMouseLeave={() => setShowProfile(false)} /> :null}
-                    </div>
-
-                    <div className={pageStyle.categories}>
-                      {categoriesArray.map((obj, index) => 
-                        <span className='categoryItem' key={index} onClick={() => dispatch(onCategoryClick(obj))} id={`${obj}`}> {obj} </span>
-                      )}
-                    </div>
+      <nav>
+        <div className={pageStyle.navbar}>
+          <div className={pageStyle.container}>
+            <div className={pageStyle.logo}>
+              <a href="/">
+                <div className={pageStyle.brandName}>
+                  <img style={{ width: 105 }} src={Logo} alt="logo" />
                 </div>
-            </nav>
-          <Outlet/> 
+              </a>
+            </div>
+
+            {location.pathname === "/" ? (
+              <form className={pageStyle.searchBar} onSubmit={handleSubmit}>
+                <input
+                  id="searchText"
+                  type="search"
+                  className={pageStyle.searchBox}
+                  placeholder="Search here.."
+                  onChange={(e) => {
+                    setSearchText(e.target.value);
+                  }}
+                />
+                <button id="searchBtn" className={pageStyle.searchBtn}>
+                  <i className="fa-solid fa-magnifying-glass"></i>
+                </button>
+              </form>
+            ) : null}
+
+            <div className={pageStyle.homeIcon}>
+              {location.pathname === "/favorites" ? (
+                <Link to="/">
+                  <i className="fa-solid fa-house"></i>
+                </Link>
+              ) : null}
+            </div>
+
+            <div
+              className={pageStyle.profile}
+              onMouseEnter={() => {
+                setShowProfile(true);
+              }}
+              onMouseLeave={() => setShowProfile(false)}
+            >
+              <i className="fa-solid fa-circle-user"></i>
+            </div>
+
+            {showProfile ? (
+              <Profile
+                onMouseEnter={() => setShowProfile(true)}
+                onMouseLeave={() => setShowProfile(false)}
+              />
+            ) : null}
+          </div>
+
+          <div className={pageStyle.categories}>
+            {categoriesArray.map((obj, index) => (
+              <span
+                className="categoryItem"
+                key={index}
+                onClick={() => dispatch(onCategoryClick(obj))}
+                id={`${obj}`}
+              >
+                {obj}
+              </span>
+            ))}
+          </div>
+        </div>
+      </nav>
+      <Outlet />
     </>
-)}
+  );
+};
 
 export default Navbar;
