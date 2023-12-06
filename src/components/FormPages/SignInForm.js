c895c2d880402ce5f029ce3c999a8e7a17131738
sanjaymohansingh <sanjaysingh19p@gmail.com>
2023-12-06 04:33:37 -0800

    signup and login form styling

--- src/components/FormPages/SignInForm.js
+++ src/components/FormPages/SignInForm.js
@@ -1,24 +1,26 @@
 import formStyle from "./formStyle.module.css";
-
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, Link } from 'react-router-dom';
-
-import { logInAsync, signInWithGoogle, userSelector } from '../../redux/reducers/userReducer';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
+import {
+  logInAsync,
+  signInWithGoogle,
+  userSelector,
+} from "../../redux/reducers/userReducer";
 
 const SignInForm = () => {
-  const [values, setValues] = useState({name:"", email:"", pass:"", confirmPass:""});
+  const [values, setValues] = useState({ email: "", pass: "" });
   const dispatch = useDispatch();
   const user = useSelector(userSelector);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
-      navigate('/'); 
+      navigate("/");
     }
   }, [user, navigate]);
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
     dispatch(logInAsync(values));
   }
@@ -26,20 +28,48 @@ const SignInForm = () => {
   return (
     <div>
       <div className={formStyle.pageStyle}>
-        <h1> Sign  In </h1>
-        <span> Watch only news you need to know. </span>
+        <h1> Sign In </h1>
+        <span style={{ fontWeight: "bold" }}>
+          {" "}
+          Watch only news you need to know.{" "}
+        </span>
         <form>
-          <input type="email" placeholder="Enter email" onChange={(e) => setValues((prev) => ({...prev, email:e.target.value}))} required/>
-          <input type="passwrord" placeholder="Enter password" onChange={(e) => setValues((prev) => ({...prev, pass:e.target.value}))} required/>
-          <button type='submit' onClick={handleSubmit}> Sign In </button>
-          <span className={formStyle.option}> or </span>
-          <button type='button' onClick={() => dispatch(signInWithGoogle())}> <span> Sign In with Google </span> </button>
+          <input
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) =>
+              setValues((prev) => ({ ...prev, email: e.target.value }))
+            }
+            required
+          />
+          <input
+            type="password"
+            placeholder="Enter password"
+            onChange={(e) =>
+              setValues((prev) => ({ ...prev, pass: e.target.value }))
+            }
+            required
+          />
+          <button type="submit" onClick={handleSubmit}>
+            Sign In
+          </button>
+          <button
+            style={{ background: "red" }}
+            type="button"
+            onClick={() => dispatch(signInWithGoogle())}
+          >
+            Sign In with Google
+          </button>
         </form>
       </div>
-
-      <div className={formStyle.otherPageLink}> <Link to='/signup' className="linkStyle">  Don't have an account? Sign Up for personalised feed. </Link> </div>
+      <div className={formStyle.otherPageLink}>
+        Don't have an account? : &nbsp;
+        <Link to="/signup" className="linkStyle">
+          Sign Up for a personalized feed.
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SignInForm;
--- src/components/FormPages/SignUpForm.js
+++ src/components/FormPages/SignUpForm.js
@@ -1,27 +1,37 @@
-import formStyle from "./formStyle.module.css";
+// SignUpForm.js
 
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, Link } from 'react-router-dom';
-import { createUserAsync, userSelector, signInWithGoogle } from '../../redux/reducers/userReducer';
+import formStyle from "./formStyle.module.css";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
+import {
+  createUserAsync,
+  userSelector,
+  signInWithGoogle,
+} from "../../redux/reducers/userReducer";
 import { toast } from "react-toastify";
 
-function SignUpForm () {
-  const [values, setValues] = useState({name:"", email:"", pass:"", confirmPass:""});
+function SignUpForm() {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    pass: "",
+    confirmPass: "",
+  });
   const dispatch = useDispatch();
   const user = useSelector(userSelector);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
-      navigate('/');
+      navigate("/");
     }
   }, [user, navigate]);
 
-  function handleSubmission(e){
+  function handleSubmission(e) {
     e.preventDefault();
-    if(values.pass !== values.confirmPass){
-      toast.error("Password and confirm password does not match.");
+    if (values.pass !== values.confirmPass) {
+      toast.error("Password and confirm password do not match.");
       return;
     }
     dispatch(createUserAsync(values));
@@ -31,23 +41,64 @@ function SignUpForm () {
     <div>
       <div className={formStyle.pageStyle}>
         <h1> Sign Up </h1>
-
         <form onSubmit={handleSubmission}>
-          <input type="name" placeholder="Enter Name" onChange={(e) => setValues((prev) => ({...prev, name:e.target.value}))} required/>
-          <input type="email" placeholder="Enter email" onChange={(e) => setValues((prev) => ({...prev, email:e.target.value}))} required/>
-          <input type="passwrord" placeholder="Enter password" onChange={(e) => setValues((prev) => ({...prev, pass:e.target.value}))} required/>
-          <input type="passwrord" placeholder="Confirm password" onChange={(e) => setValues((prev) => ({...prev, confirmPass:e.target.value}))} required/>
-          
-          <button> Sign Up </button>
-          <button onClick={() => dispatch(signInWithGoogle())}> 
-            <span> Sign In with Google </span> 
-        </button>
+          <input
+            type="text"
+            placeholder="Enter Name"
+            onChange={(e) =>
+              setValues((prev) => ({ ...prev, name: e.target.value }))
+            }
+            required
+          />
+          <input
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) =>
+              setValues((prev) => ({ ...prev, email: e.target.value }))
+            }
+            required
+          />
+          <input
+            type="password"
+            placeholder="Enter password"
+            onChange={(e) =>
+              setValues((prev) => ({ ...prev, pass: e.target.value }))
+            }
+            required
+          />
+          <input
+            type="password"
+            placeholder="Confirm password"
+            onChange={(e) =>
+              setValues((prev) => ({ ...prev, confirmPass: e.target.value }))
+            }
+            required
+          />
+          <button type="submit" className={formStyle.signUpButton}>
+            Sign Up
+          </button>
+          <button
+            style={{ background: "red" }}
+            type="button"
+            onClick={() => dispatch(signInWithGoogle())}
+          >
+            Sign In with Google
+          </button>
         </form>
       </div>
-      
-      <div className={formStyle.otherPageLink}><Link to='/signin' className="linkStyle"> Already have an account? Sign in. </Link></div>
+      <div className={formStyle.otherPageLink}>
+        Already have an account? : &nbsp;
+        <Link
+          to="/signin"
+          className={`linkStyle ${
+            window.location.pathname === "/signin" ? "active" : ""
+          }`}
+        >
+          Sign in.
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SignUpForm;
